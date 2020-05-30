0f711dffa0341c2048e7d7670ffd00baf65faae5
Sujan L <sujann42@gmail.com>
2020-05-30 11:58:42 -0500

    Update LoggingToFacebook.java

--- src/main/java/com/sel_one/LoggingToFacebook.java
+++ src/main/java/com/sel_one/LoggingToFacebook.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoggingToFacebook {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "/Users/suzan/Documents/CHROME_DRIVER/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/Users/************/CHROME_DRIVER/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
 		String url = driver.getCurrentUrl();
